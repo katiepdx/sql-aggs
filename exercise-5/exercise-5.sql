750c0023f6e77c148bf487150af153c9b151d74b
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-04-20 17:48:49 -0700

    add exercise 5

--- exercise-5/exercise-5.sql
+++ exercise-5/exercise-5.sql
@@ -1,1 +1,5 @@
 -- the total run time of all inventory items if they were all played back to back
+
+SELECT SUM(length)
+FROM film
+INNER JOIN inventory ON film.film_id = inventory.film_id
--- exercise-5/exercise-5.test.js
+++ exercise-5/exercise-5.test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const pool = require('../lib/utils/pool');
 
-describe.skip('exercise 5', () => {
+describe('exercise 5', () => {
   afterAll(() => pool.end());
 
   it('find the total run time of all inventory items if they were all played back to back', async () => {
