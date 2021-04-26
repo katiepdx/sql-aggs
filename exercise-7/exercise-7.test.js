454a7fccefe1eaa420efd9a9622834cca304f211
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-04-26 11:22:20 -0700

    add exercise 7

--- exercise-7/exercise-7.test.js
+++ exercise-7/exercise-7.test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const pool = require('../lib/utils/pool');
 
-describe.skip('exercise 7', () => {
+describe('exercise 7', () => {
   afterAll(() => pool.end());
 
   it('find customer ranking by total film time watched', async () => {
